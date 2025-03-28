09838ad9464c108458cae9c8c9cd538c917a9fcc
Emerson <the.emerson.araujo@gmail.com>
2025-03-27 21:18:31 -0300

    primeiro commit

---
+++ eleitor.py
@@ -0,0 +1,10 @@
+idade = int(input('informe sua idade: '))
+
+if idade < 16:
+    print(f'Você tem {idade} e é um não eleitor!')
+elif 18 <= idade < 65:
+    print(f'Você tem {idade} e é um eleitor obrigatorio! ')
+elif (16 > idade < 18) or (idade > 65):
+    print(f'Você tem {idade} e é um eleitor facutativo')
+else:
+    print("erro")
