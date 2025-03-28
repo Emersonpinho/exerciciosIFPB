24be08ab961f70af99ed6aeb4a28121ca6b4c669
Emerson <the.emerson.araujo@gmail.com>
2025-03-27 21:20:37 -0300

    primeiro commit

--- eleitor.py
+++ eleitor.py
@@ -6,5 +6,3 @@ elif 18 <= idade < 65:
     print(f'Você tem {idade} e é um eleitor obrigatorio! ')
 elif (16 > idade < 18) or (idade > 65):
     print(f'Você tem {idade} e é um eleitor facutativo')
-else:
-    print("erro")
