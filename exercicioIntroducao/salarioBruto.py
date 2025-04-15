095cee48256db10c649e8c32d25c0dbd6c038214
emersonpinho <the.emerson.araujo@gmail.com>
2025-04-15 17:42:16 -0300

    mais um exercicio adicionado

---
+++ exercicioIntroducao/salarioBruto.py
@@ -0,0 +1,6 @@
+horasTrab = float(input('Digite quantas horas foram trabalhadas: '))
+valorHora = float(input('Digite o valor da hora trabalhada: '))
+
+salarioBruto = horasTrab * valorHora
+
+print(f'Seu salario Bruto é {salarioBruto}')
