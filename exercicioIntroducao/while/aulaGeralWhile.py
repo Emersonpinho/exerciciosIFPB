f39a72ed11742496e428f50580f3c627ccebc0fb
Emerson pinho <the.emerson.araujo@gmail.com>
2025-05-29 19:14:29 -0300

    vamos para mudanças

--- exercicioIntroducao/while/aulaGeralWhile.py
+++ exercicioIntroducao/while/aulaGeralWhile.py
@@ -6,7 +6,15 @@ while c < 10:
 
 print('fim')'''
 
-r = 'sim'
-while r == 'sim':
-    r = input('Deseja continuar?(sim/ não) ').lower()
-print('fim')
+n = 1
+par = impar = c = 0
+
+while n != 0:
+    n = int(input('Digite um valor: '))
+    c += 1
+    if n % 2 == 0:
+        par += 1
+    else:
+        impar += 1
+
+print(f'Relatorio:\nNúmeros digitados: {c-1}\nNúmeros PARES: {par-1}\nNúmeros IMPARES {impar}')
