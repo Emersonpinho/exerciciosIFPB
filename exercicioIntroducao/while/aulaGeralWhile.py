3d13be420a9fd8c33e593e468e04820b0a19dc11
Emerson pinho <the.emerson.araujo@gmail.com>
2025-05-29 19:17:03 -0300

    tirei a minha gambiarra :)

--- exercicioIntroducao/while/aulaGeralWhile.py
+++ exercicioIntroducao/while/aulaGeralWhile.py
@@ -12,9 +12,13 @@ par = impar = c = 0
 while n != 0:
     n = int(input('Digite um valor: '))
     c += 1
-    if n % 2 == 0:
-        par += 1
-    else:
-        impar += 1
+    
+    if n != 0:
+
+        if n % 2 == 0:
+            par += 1
+            
+        else:
+            impar += 1
 
 print(f'Relatorio:\nNúmeros digitados: {c-1}\nNúmeros PARES: {par-1}\nNúmeros IMPARES {impar}')
