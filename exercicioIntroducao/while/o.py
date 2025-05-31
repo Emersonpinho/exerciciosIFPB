a5c47605ffa2d51120bfa5a786ae13563e185fdb
Emerson pinho <the.emerson.araujo@gmail.com>
2025-05-31 10:21:42 -0300

    mudanças

---
+++ exercicioIntroducao/while/o.py
@@ -0,0 +1,10 @@
+n = int(input("Digite o valor de N: "))
+
+i = n
+while i >= 1:
+    j = i
+    while j >= 1:
+        print(j, end=" ")
+        j -= 1
+    print()
+    i -= 1
---
+++ exercicioIntroducao/while/somatorio.py
@@ -0,0 +1,10 @@
+c = 0
+soma = 0
+
+while c < 20:
+    valor = float(input('Digite um número: '))
+    c += 1
+
+    soma += valor
+
+print(soma)
