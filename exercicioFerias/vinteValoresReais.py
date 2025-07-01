5f8f4928ea123966191a85c39960bd59a7d9d9b5
Emerson pinho <the.emerson.araujo@gmail.com>
2025-07-01 16:40:14 -0300

    mais um exercicio adicionado

---
+++ exercicioFerias/vinteValoresReais.py
@@ -0,0 +1,9 @@
+soma = 0
+cont = 0
+
+while cont < 20:
+    numero = int(input('Digite um número: '))
+    soma = soma + numero
+    cont += 1
+
+print(f'A soma dos vinte valores digitados é {soma}')
