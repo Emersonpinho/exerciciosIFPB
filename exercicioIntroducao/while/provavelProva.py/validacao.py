a97df406135527767139449df73c885f8aadce31
Emerson <the.emerson.araujo@gmail.com>
2025-07-29 17:08:10 -0300

    algumas considerações

--- exercicioIntroducao/while/provavelProva.py/validacao.py
+++ exercicioIntroducao/while/provavelProva.py/validacao.py
@@ -17,11 +17,11 @@ salario = float(input('Digite o valor do seu salario: $'))
 while salario <= 0:
     salario = float(input('salario precisa ser maior que zero: $'))
 
-sexo = input('Digite seu Sexo (m, f):') 
+sexo = input('Digite seu Sexo (m, f):').strip().lower()
 while (sexo != 'm') and (sexo != 'f'):
     sexo = input('ERRO: sexo invalido. Digite seu Sexo (m, f): ')
 
-estadoCivil = input('Digite seu estado civil (c, s, v, d):')
+estadoCivil = input('Digite seu estado civil (c, s, v, d):').strip().lower()
 while estadoCivil not in estadoCivilValido:
     estadoCivil = input('ERRO estado civil não reconhecido! Digite seu estado civil (c, s, v, d):')
 
