a6261ca041ef1a1c465f369c600ef0c242f7eeda
Emerson Pinho <the.emerson.araujo@gmail.com>
2025-10-25 10:07:31 -0300

    mais exercicios

---
+++ dicionario/main.py
@@ -0,0 +1,7 @@
+l = ['pedro', 1000, 'carlos', 750, 'ricardo', 2500]
+
+d = {'pedro': 1000,
+     'carlos': 750,
+     'ricardo': 2500}
+
+print(len(d))
---
+++ dicionario/pegarVlaor.py
@@ -0,0 +1,7 @@
+d = {'pedro': 1000,
+     'carlos': 750,
+     'ricardo': 2500}
+
+for item in d:
+    valor = item
+    print(d[valor])
