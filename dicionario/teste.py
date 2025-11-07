25aa5abe1d9b23d35bfb3362ac5303a701df85d1
Emerson pinho <the.emerson.araujo@gmail.com>
2025-11-06 22:06:41 -0300

    mais um exercicio

---
+++ dicionario/teste.py
@@ -0,0 +1,12 @@
+pessoa = {
+    'Nome': 'Emerson',
+    'idade': '16',
+    'sexo': 'Masculino',
+    'cor': 'Preto',
+}
+chave = (input('Digite a chave: '))
+valor = (input('Digite o valor:'))
+
+p = dict(chave=valor) 
+
+print(p)
