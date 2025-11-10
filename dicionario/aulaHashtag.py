8733396c4f789df20d25eec5462612dc57decbf9
Emerson pinho <the.emerson.araujo@gmail.com>
2025-11-09 21:12:39 -0300

    mais exercicio adicionado

---
+++ dicionario/aulaHashtag.py
@@ -0,0 +1,13 @@
+emais_gerentes = {
+    'iguatemi': 'iguatemi@gmail.com',
+    'plaza': 'plaza@gmail.com'
+}
+
+emais_gerentes['lebon'] = 'Lebon@gmail.com'
+
+'''for item in emais_gerentes:
+    print(f'{item} : {emais_gerentes[item]}')'''
+
+print(emais_gerentes.values())
+
+print(emais_gerentes)
